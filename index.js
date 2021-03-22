ae37da555bd34d29322e6f492900bd02c929552c
Şafak Usta <78078276+safakcosplay@users.noreply.github.com>
2021-03-22 13:00:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function startGame() {
+  var randomNumber1 = (Math.floor(Math.random()*6) + 1);
+  var randomNumber2 = (Math.floor(Math.random()*6) + 1);
+  document.querySelector(".img1").src = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img2").src = "images/dice" + randomNumber2 + ".png";
+  if(randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Wins!";
+  }
+  else if(randomNumber2 > randomNumber1) {
+    document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector('h1').innerHTML = "Draw!";
+  }
+}
+
+document.getElementById('roll').onclick = startGame;
